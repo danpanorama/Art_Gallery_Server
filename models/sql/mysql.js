b3c14c8ba616df531316713a0e02387b58052aba
danpanorama <62213659+danpanorama@users.noreply.github.com>
2022-11-26 13:23:02 +0200

    sql config change

--- models/sql/mysql.js
+++ models/sql/mysql.js
@@ -4,11 +4,13 @@ require('dotenv').config();
 let pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
+  port:process.env.PORT,
   password:process.env.PASSWORD,
   database:process.env.DATABASE,
   waitForConnections: true, 
   connectionLimit: 10,
   queueLimit: 0,
 });
+
 
 module.exports = pool.promise();
